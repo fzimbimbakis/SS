03170ee2cac45a14f6fe5dc1b1dde5f2bda988a7
bsquillari <bsquillari@itba.edu.ar>
2023-03-26 22:51:30 -0300

    TP2: add: Create animation from frames

---
+++ TP2/src/main/python/create_animation_from_frames.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import imageio
+# Ruta a la carpeta que contiene las imágenes
+animation_frames_folder = '../resources/animation/case1/frames'
+frames_files_name_format = 'animation*.png'
+animation_name = '../resources/animations/case1/animation.mp4'
+
+
+def main():
+
+    # Obtener la lista de archivos de imagen en la carpeta
+    images = sorted(glob.glob(os.path.join(animation_frames_folder, frames_files_name_format)))
+
+    # Crear la animación con las imágenes en la lista
+    with imageio.get_writer(animation_name, fps=30) as writer:
+        for image in images:
+            # Leer la imagen
+            img = imageio.imread(image)
+
+            # Añadir la imagen al archivo de video
+            writer.append_data(img)
+
+    print('La animación se ha guardado correctamente en ' + animation_name)
+
+
+if __name__ == "__main__":
+    main()
