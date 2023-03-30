198e659118041e4cbb3b5ab72e3b7d32d6444144
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2023-03-30 20:18:27 -0300

    fix: vaVsTime graph

--- TP2/src/main/python/noise_analysis/vaVsTime.py
+++ TP2/src/main/python/noise_analysis/vaVsTime.py
@@ -11,17 +11,19 @@ def read_vas_file(name):
     return list_x , list_y
 
 
-
-
 if __name__ == '__main__':
+    plt.xlabel('Iteraciones')
+    plt.ylabel('Va')
+    plt.set_ylim([0.0, 1.1])
 
-    for j in [i / 10 for i in range(0, 81, 2)]:
-        time, va = read_vas_file("../../resources/noise_analysis/vaVsTime(noise="+ str(j)+ ").txt")
-        fig, ax = plt.subplots()
-        ax.plot(time, va)
-        plt.title('Ruido = ' + str(j))
-        plt.xlabel('Iteraciones')
-        plt.ylabel('Va')
-        ax.set_ylim([0.0, 1.1])
-        plt.savefig("../../resources/noise_analysis/graphs/vaVsTime(noise="+str(j)+").png")
-        plt.close()
+    time, va = read_vas_file("../../resources/noise_analysis/vaVsTime(noise=0.2).txt")
+    plt.plot(time, va, "g", label="η = 0.6")
+    time, va = read_vas_file("../../resources/noise_analysis/vaVsTime(noise=2.0).txt")
+    plt.plot(time, va, "r", label="η = 2.4")
+    time, va = read_vas_file("../../resources/noise_analysis/vaVsTime(noise=3.5).txt")
+    plt.plot(time, va, "b", label="η = 3.6")
+    time, va = read_vas_file("../../resources/noise_analysis/vaVsTime(noise=5.0).txt")
+    plt.plot(time, va, "k", label="η = 5.0")
+
+    plt.legend(loc='upper right')
+    plt.savefig("../../resources/noise_analysis/graphs/vaVsTime.png")
